4c0a2d7baa11da73bee3d7566d9eeb8d8534e53e
Ryan Cleven <rcleven@mac.com>
2023-04-18 10:27:17 -0700

    Added attribution

--- src/hsig/Signal.hx
+++ src/hsig/Signal.hx
@@ -10,6 +10,7 @@
 	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+// Heavily derived from https://github.com/peteshand/signals, with modification by https://github.com/onehundredfeet
 
 package hsig;
 
